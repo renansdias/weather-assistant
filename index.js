07acca806b9089eb814c32375a8931ecca65b22a
Renan Dias <renansdiaz@gmail.com>
2019-08-07 11:16:03 -0400

    First commit - Weather Assistant

---
+++ index.js
@@ -0,0 +1,21 @@
+const express        = require('express')
+var nodejsWeatherApp = require('nodejs-weather-app');
+const app            = express()
+const port           = 3000
+
+app.get('/weather', (req, res) => {
+    if (req.query.city === undefined) {
+        throw new Error("Error: use city as query string")
+    } else {
+        nodejsWeatherApp.getWeather(req.query.city).then(weather => {
+            if (weather === undefined) {
+                throw new Error(`City ${req.query.city} not found!`)
+            }
+            console.log(`Temperature in ${weather.name} is ${weather.main.temp}°C`)
+        });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
